61eae69a3498760aefd45b26f59a1400af82e3f8
Michael McHugh <mmchugh@gmail.com>
2015-07-15 20:59:30 -0700

    whoa it worked

--- glhf.pyx
+++ glhf.pyx
@@ -1,2 +1,3 @@
 cdef extern from "glfw/include/GLFW/glfw3.h":
     cpdef int glfwInit()
+    cpdef void glfwTerminate()
--- setup.py
+++ setup.py
@@ -1,6 +1,47 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+
+sourcefiles = [
+    "glhf.pyx",
+    "glfw/src/glx_context.c",
+    "glfw/src/x11_init.c",
+    "glfw/src/x11_monitor.c",
+    "glfw/src/x11_window.c",
+    "glfw/src/xkb_unicode.c",
+    "glfw/src/linux_joystick.c",
+    "glfw/src/posix_time.c",
+    "glfw/src/posix_tls.c",
+    "glfw/src/context.c",
+    "glfw/src/init.c",
+    "glfw/src/input.c",
+    "glfw/src/monitor.c",
+    "glfw/src/window.c",
+]
+
+extensions = [
+    Extension(
+        "glhf",
+        sources=sourcefiles,
+        define_macros=[
+            ("_GLFW_USE_OPENGL", None),
+            ("_GLFW_X11", None),
+            ("_GLFW_GLX", None),
+            ("_GLFW_HAS_GLXGETPROCADDRESS", None),
+        ],
+        libraries=[
+            "GL",
+            "X11",
+            "Xrandr",
+            "Xxf86vm",
+            "Xi",
+            "Xcursor",
+            "m",
+            "Xinerama",
+        ],
+    )
+]
 
 setup(
-    ext_modules=cythonize("glhf.pyx")
+    ext_modules=cythonize(extensions)
 )
