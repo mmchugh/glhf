531d2a0f4c6d7ef0137809bfa3463c9aae8cf345
mmchugh <michael.mchugh@urbanairship.com>
2015-07-16 20:41:59 -0700

    window creation

---
+++ glfw.pxd
@@ -0,0 +1,15 @@
+cdef extern from "glfw/include/GLFW/glfw3.h":
+    ctypedef struct GLFWwindow:
+        pass
+
+    ctypedef struct GLFWmonitor:
+        pass
+
+    int glfwInit()
+    void glfwTerminate()
+
+    GLFWwindow* glfwCreateWindow(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share)
+    void glfwSwapBuffers(GLFWwindow* window)
+    void glfwPollEvents()
+    void glfwMakeContextCurrent(GLFWwindow* window)
+    void glfwSwapInterval(int interval)
--- glhf.pyx
+++ glhf.pyx
@@ -1,3 +1,27 @@
-cdef extern from "glfw/include/GLFW/glfw3.h":
-    cpdef int glfwInit()
-    cpdef void glfwTerminate()
+import atexit
+
+cimport glfw
+
+# Initialize glfw on import, because we can do that in python
+init = glfw.glfwInit()
+if not init:
+    raise Exception("Couldn't initialize glfw")
+
+# make sure we call terminate when we quit
+atexit.register(glfw.glfwTerminate)
+
+cdef glfw.GLFWwindow* _window = NULL
+
+def open_window(width, height, title):
+    global _window
+    _window = glfw.glfwCreateWindow(width, height, title, NULL, NULL)
+
+    #set this window as the opengl context
+    glfw.glfwMakeContextCurrent(_window)
+
+    #enable vsync
+    glfw.glfwSwapInterval(1)
+
+def finish_frame():
+    glfw.glfwSwapBuffers(_window)
+    glfw.glfwPollEvents()
